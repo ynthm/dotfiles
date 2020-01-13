50546a03dda5a1f48d46da626113d50030890d4c
Ynthm Wang <ynthm.w@gmail.com>
2020-01-13 20:28:53 +0800

    update

--- config/java/path.zsh
+++ config/java/path.zsh
@@ -1,2 +1,15 @@
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
+function setjdk() {
+    export JAVA_HOME=`/usr/libexec/java_home -v $@`
+    export PATH=$JAVA_HOME/bin:$PATH
+    export CLASS_PATH=$JAVA_HOME/lib
+}
+
+function jdk8(){
+    setjdk 1.8
+}
+function jdk11(){
+    setjdk 11
+}
+function java-v(){
+    /usr/libexec/java_home -V
+}
