8f28899ab300f68c51659526da42fbebbe99ba2a
Ynthm Wang <ynthm.w@gmail.com>
2020-08-05 11:39:52 +0800

    ssh config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env bash
 
 # 这条命令写在脚本文件里才有作用 dirname $0 指当前脚本所在目录
-DOTFILES_ROOT=$(cd "$(dirname "$0")";pwd)
+DOTFILES_ROOT=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 # echo `pwd`
 
-info () {
+info() {
   printf "\r[\033[00;36mINFO\033[0m] $1\n"
 }
 # 用户选择
-user () {
+user() {
   printf "\r[ \033[0;33m??\033[0m ] $1\n"
 }
 
-ok () {
+ok() {
   printf "\r\033[2K[ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail () {
+fail() {
   printf "\r\033[2K[\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
 }
 
-install_homebrew () {
-  if test ! $(which brew)
-  then
+install_homebrew() {
+  if test ! $(which brew); then
     info "Installing Homebrew for you..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   else
@@ -34,9 +36,8 @@ install_homebrew () {
 
 install_homebrew
 
-install_ohmyzsh () {
-  if [ ! -d ~/.oh-my-zsh ]
-  then
+install_ohmyzsh() {
+  if [ ! -d ~/.oh-my-zsh ]; then
     info 'Installing oh-my-zsh'
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
   else
@@ -46,23 +47,18 @@ install_ohmyzsh () {
 
 install_ohmyzsh
 
-
-if [ "$SHELL" != "/bin/zsh" ]
-  then
-    chsh -s $(which zsh)
+if [ "$SHELL" != "/bin/zsh" ]; then
+  chsh -s $(which zsh)
 fi
 
-
-install_brew_apps () {
+install_brew_apps() {
   # If we're on a Mac, let's install and setup homebrew.
-  if [ "$(uname -s)" == "Darwin" ]
-  then
+  if [ "$(uname -s)" == "Darwin" ]; then
     info "Installing dependencies"
     # 执行其他脚本
     cd $DOTFILES_ROOT
     cd script
-    if source brew.sh
-    then
+    if source brew.sh; then
       ok "Dependencies installed"
     else
       fail "Error installing dependencies"
@@ -72,26 +68,21 @@ install_brew_apps () {
 
 install_brew_apps
 
-
-
-link_file () {
+link_file() {
   local src=$1 dst=$2
 
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-  then
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
 
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
-    then
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
 
       local currentSrc="$(readlink $dst)"
 
-      if [ "$currentSrc" == "$src" ]
-      then
+      if [ "$currentSrc" == "$src" ]; then
 
-        skip=true;
+        skip=true
 
       else
 
@@ -100,20 +91,26 @@ link_file () {
         read -n 1 action
 
         case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
+        o)
+          overwrite=true
+          ;;
+        O)
+          overwrite_all=true
+          ;;
+        b)
+          backup=true
+          ;;
+        B)
+          backup_all=true
+          ;;
+        s)
+          skip=true
+          ;;
+        S)
+          skip_all=true
+          ;;
+        *) ;;
+
         esac
 
       fi
@@ -124,34 +121,29 @@ link_file () {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]
-    then
+    if [ "$overwrite" == "true" ]; then
       rm -rf "$dst"
       ok "removed $dst"
     fi
 
-    if [ "$backup" == "true" ]
-    then
+    if [ "$backup" == "true" ]; then
       mv "$dst" "${dst}.backup"
       ok "moved $dst to ${dst}.backup"
     fi
 
-    if [ "$skip" == "true" ]
-    then
+    if [ "$skip" == "true" ]; then
       ok "skipped $src"
     fi
   fi
 
-  if [ "$skip" != "true" ]  # "false" or empty
-  then
+  if [ "$skip" != "true" ]; then # "false" or empty
     ln -s "$1" "$2"
     ok "linked $1 to $2"
   fi
 }
 
-link () {
-  if [ ! -d $HOME/.config/youtube-dl/config ]
-  then
+link() {
+  if [ ! -d $HOME/.config/youtube-dl/config ]; then
     info "config youtube-dl"
     mkdir ~/.config/youtube-dl
     ln -s "$DOTFILES_ROOT/config/youtube-dl/config" "$HOME/.config/youtube-dl/config"
@@ -164,8 +156,7 @@ install_dotfiles() {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find "$DOTFILES_ROOT/config" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-  do
+  for src in $(find "$DOTFILES_ROOT/config" -maxdepth 2 -name '*.symlink' -not -path '*.git*'); do
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
   done
@@ -173,9 +164,8 @@ install_dotfiles() {
 
 install_dotfiles
 
-install_spacevim () {
-  if [ ! -d ~/.SpaceVim.d ]
-  then
+install_spacevim() {
+  if [ ! -d ~/.SpaceVim.d ]; then
     info "Installing SpaceVim for you..."
     curl -sLf https://spacevim.org/cn/install.sh | bash
   else
--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -16,4 +16,12 @@ source "$HOMEBREW_FOLDER/zsh-autosuggestions/zsh-autosuggestions.zsh"
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
 export HOMEBREW_NO_AUTO_UPDATE=true
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
+# Configuring Completions in zsh
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+
+  autoload -Uz compinit
+  compinit
+fi
