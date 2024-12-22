b178b87dbc53210e45904458f111400ff0fedee1
Ynthm Wang <ynthm.w@gmail.com>
2024-12-22 15:52:26 +0800

    update

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -1,3 +1,8 @@
+# export DOCKER_HOST=tcp://10.0.0.30:2375
+# export http_proxy=http://10.0.0.30:7897
+# export https_proxy=http://10.0.0.30:7897
+# export no_proxy=localhost,127.0.0.1
+
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:~/Apps/bin
 export PATH=$PATH:~/Apps/bin/rspleeter
@@ -5,8 +10,9 @@ export HOMEBREW_NO_AUTO_UPDATE=true
 # homebrew install library
 export PATH="/opt/homebrew/bin:$PATH"
 # go
-export GOPATH=/usr/local/go
-export PATH=$PATH:$GOPATH/bin
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 # postgresql
 # export PATH="/opt/homebrew/opt/postgresql@9.6/bin:$PATH"
 
@@ -17,8 +23,8 @@ export PATH=$PATH:/Users/ynthm/Apps/Hadoop/hadoop-3.3.2/bin:/Users/ynthm/Apps/Ha
 
 # Node.js
 export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # autojump
 [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
